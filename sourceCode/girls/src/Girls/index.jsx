56b86067cd75c02afa3dc28179d1b8a5b1e7df81
Your Name <you@example.com>
2019-08-08 16:19:51 +0800

    why-create

--- sourceCode/girls/src/Girls/index.jsx
+++ sourceCode/girls/src/Girls/index.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter , Route , Switch , Redirect } from 'react-router-dom'
 // 引入组件
 import Login from './Entry/login/index.jsx'
 import Registe from './Entry/registe/index.jsx'
+import CreateLogin from './Entry/up-to-date/login'
 
 export default class Home extends Component {
     render() {
